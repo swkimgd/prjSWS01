aa32c453a56d59670d37f8e56a4cb009d3d38377
swkimgd <kimgd@smartwide.co.jp>
2025-07-30 22:20:36 +0900

    メンバー管理ページ作成

--- memberMng/urls.py
+++ memberMng/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'memberMng'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('members/', views.MemberListView.as_view(), name='members'),  # メンバー管理ページへのURL
 ]
+
+
+
+
 
 
--- memberMng/views.py
+++ memberMng/views.py
@@ -2,3 +2,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class MemberListView(TemplateView):
+    template_name = 'members.html'
+    
